2e887370124a0a282355df4721cd0723b1532809
jeanpier <jeanpi.jpct@gmail.com>
2024-02-13 16:58:28 +0100

    cambios de url de imagen

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        remotePatterns:[{
-            protocol:'http',
-            hostname:'127.0.0.1',
-            port:'8000',
-            pathname:'/storage/**'
-        }]
+        remotePatterns:[
+            {
+                protocol:'http',
+                hostname:'127.0.0.1',
+                port:'8000',
+                pathname:'/storage/**'
+            },
+            {
+                protocol:'https',
+                hostname:'https://apirmd.herramientaswp.com',
+                pathname:'/storage/products/**'
+            }
+        ]
     }
 };
 
