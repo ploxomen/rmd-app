4a1d397e694324a512bc806584935c1a16c73536
jeanpier <jeanpi.jpct@gmail.com>
2024-09-25 18:02:03 +0200

    estableciendo .env produccion a su ruta real
    quitando la validadcion de vencimiento de inicio de sesion

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 import '@/app/globals.css';
-import useIdleLogout from '@/hooks/useIdleLogout';
 
 const MyApp = ({ Component, pageProps }) => {
-  const router = useRouter();
-
-  // No aplicar el hook en la página de login
-  const isLoginPage = router.pathname === '/login';
-
-  if (!isLoginPage) {
-    useIdleLogout();
-  }
-
   return <Component {...pageProps} />;
 };
 
