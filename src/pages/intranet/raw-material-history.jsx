ae7e284bae6cb3063bbe46658ffa504ee91ff365
Jean Pier Carrasco Tamariz <jeanpi.jpct@gmail.com>
2025-01-14 01:22:37 +0100

    cambios

--- src/pages/intranet/raw-material-history.jsx
+++ src/pages/intranet/raw-material-history.jsx
@@ -247,7 +247,7 @@ function RawMaterialHistory({
         ) : (
           <div className="w-full p-6 bg-white rounded-md shadow overflow-x-auto">
             <div className="flex items-center justify-between gap-2 flex-wrap mb-2">
-              <div style={{ width: "200px" }}>
+              <div style={{ width: "190px" }}>
                 <Link
                   href="/intranet/raw-material"
                   className="rounded-md overflow-hidden relative inline-flex group items-center justify-center px-3.5 py-2 m-1 cursor-pointer border-b-4 border-l-2 hover:bg-red-600 font-semibold transition-all ease-in-out shadow-lg bg-gradient-to-tr bg-red-500 text-white"
